a74aaa0e17a163be036a7b74e1c4b3742de55fbe
zacscodingclub <zbaston@gmail.com>
2017-10-21 15:19:04 -0700

    through basic variables;

---
+++ basic-package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/zacscodingclub/learning-go-basics/basic-package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG , olleH"))
+	fmt.Println(stringutil.MyName)
+}
---
+++ variables/basic/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+	// var a int
+	// var b string
+	// var c float64
+	// var d bool
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+}
